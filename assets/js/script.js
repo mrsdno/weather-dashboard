1e538fe48a544caa73d1c7c349b2a93d7cec4440
mrsdno <christine@dno.dev>
2022-04-12 09:16:01 -0400

    add search button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,39 @@
+var userFormEl = document.querySelector("#user-form");
 var selectedCityEl = document.querySelector(".selected-city-container");
+var cityStateEl = document.querySelector("#location");
 var apiKey = "e53b451ac79065e81383f7950853a84e"
 
-var getLatLon = function() {
-    var cityState = document.querySelector("#location");
-    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityState + "&limit={limit}&appid=" + apiKey;
+
+var getLatLon = function(location) {
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + location + "&limit=5&appid=" + apiKey;
+    
+    fetch(apiUrl).
+    then(function(response) {
+        if (response.ok) {
+            response.json().then(function(response){
+                console.log(response);
+            })
+        }
+        else {
+            alert("Error: City/State not found.")
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to weather data.")
+    })
+}
+
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    var cityStateSpaces = cityStateEl.value.trim();
+    var cityState = cityStateSpaces.split(' ').join('');
+
+    if (cityState) {
+        getLatLon(cityState);
+        cityStateEl.value="";
+
+    }
 }
 
 var getWeatherData = function() {
@@ -47,6 +77,9 @@ var displayWeatherData = function(response) {
     selectedCityEl.appendChild(currentHumidityEl)
     selectedCityEl.appendChild(currentUVEl)
 }
+
+
+userFormEl.addEventListener("click", formSubmitHandler);
 
 getWeatherData();
 
